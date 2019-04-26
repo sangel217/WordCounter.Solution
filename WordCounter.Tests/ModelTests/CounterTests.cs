3144fe04bc59cb29734de4e8a0ca98b6777af0d8
Sarah Angel <sangel217@hotmail.com>
2019-04-26 16:11:48 -0700

    add 5 successful test methods

---
+++ WordCounter.Tests/ModelTests/CounterTests.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class CounterTest
+  {
+
+    [TestMethod]
+    public void CounterConstructor_CreatesOfConstructor_Counter()
+    {
+      Counter newObject = new Counter("test", "test");
+      Assert.AreEqual(typeof(Counter), newObject.GetType());
+    }
+
+    [TestMethod]
+    public void GetCounterObeject_ReturnsWord_String()
+    {
+      string inputWord = "hello";
+      string sentence = "hello my name is sarah";
+      Counter newObject = new Counter(inputWord, sentence);
+      string result = newObject.GetCounterWord();
+      Assert.AreEqual(inputWord, result);
+    }
+
+    [TestMethod]
+    public void GetCounterSentence_ReturnsSentence_String()
+    {
+      string sentence = "hello my name is sarah";
+      string inputWord = "hello";
+      Counter newObject = new Counter(inputWord, sentence);
+      string result = newObject.GetCounterSentence();
+      Assert.AreEqual(sentence, result);
+    }
+
+    [TestMethod]
+    public void  MakeLowerCaseWord_ReturnsLowerCase_String()
+    {
+      string inputWord = "HELLO";
+      string sentence = "HEllo My Name iS Sarah";
+      Counter newObject = new Counter(inputWord, sentence);
+      string result = newObject.MakeLowerCaseWord();
+      string expected = "hello";
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void  MakeLowerCaseSentence_ReturnsLowerCase_String()
+    {
+      string inputWord = "HELLO";
+      string sentence = "HEllo My Name iS Sarah";
+      Counter newObject = new Counter(inputWord, sentence);
+      string result = newObject.MakeLowerCaseSentence();
+      string expected = "hello my name is sarah";
+      Assert.AreEqual(expected, result);
+    }
+  }
+}
